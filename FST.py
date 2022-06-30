83eaa5781ffa3762110d9baa4c050ff1bddec7fa
mohammad hashemy <smhd.hashemy@outlook.com>
2022-06-30 14:32:08 +0430

    add landa transition

--- FST.py
+++ FST.py
@@ -11,20 +11,26 @@ class FST:
         if in_state_name in self.sts and out_stat_name in self.sts:
             self.trans.append(transition(in_state_name, input, output, out_stat_name))
         else:
-            print("no such transition name",in_state_name,out_stat_name)
+            print("no such state name", in_state_name, out_stat_name)
 
     def add_set_transition(
-        self, in_state_name: str, input: str, output: str, out_stat_name: str
+        self, in_state_name: str, input_set: str, out_stat_name: str
     ) -> None:
-        for inp, out in zip(input, output):
-            self.add_transition(in_state_name, inp, out, out_stat_name)
-        
+        for inp in input_set:
+            self.add_transition(in_state_name, inp, inp, out_stat_name)
 
     def __is_final(self, state_name: str) -> bool:
         for s in self.sts:
             if s.name == state_name:
                 return s.is_final
         return False
+
+    def __landa_transition(self, to_p: list) -> list[str]:
+        for tp in to_p:
+            for t in self.trans:
+                if t.input == "" and tp[1] == t.in_state_name:
+                    to_p.append((tp[0] + t.output, t.out_stat_name))
+        return to_p
 
     def run(self, inp: str) -> list[str]:
         to_p = [("", "q0")]  # list to pars
@@ -34,6 +40,7 @@ class FST:
                 for t in self.trans:
                     if tp[1] == t.in_state_name and c == t.input:
                         p_out.append((tp[0] + t.output, t.out_stat_name))
+            p_out = self.__landa_transition(p_out)
             to_p = p_out
             p_out = []
         return [i[0] for i in to_p if self.__is_final(i[1])]
@@ -69,3 +76,15 @@ class transition:
         self.input = input
         self.output = output
         self.out_stat_name = out_stat_name
+
+    def __str__(self) -> str:
+        return (
+            self.in_state_name
+            + "->"
+            + self.out_stat_name
+            + "("
+            + self.input
+            + ","
+            + self.output
+            + ")"
+        )
