6106cbf86b47a54b4f2c8c9b0c1776c7e3246319
mohammad hashemy <smhd.hashemy@outlook.com>
2022-07-01 21:04:17 +0430

    add normal nones

--- morphological.py
+++ morphological.py
@@ -4,6 +4,7 @@ fst = FST()
 AZ = "abcdefghijklmnopqrstuvwxyz"
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+NONSPECIAL = "abcdgijklmnpqrtuvw"
 
 fst.add_state("q0", False)
 fst.add_set_transition("q0", AZ, "q0")
@@ -20,13 +21,10 @@ fst.add_transition("q0", "o", "o", "qes")
 fst.add_transition("qes", "", "e", "qs")
 
 # ##[sc]h -> ##[sc]hes
-fst.add_state("qch", False)
-fst.add_state("qsh", False)
-fst.add_transition("q0", "c", "c", "qch")
-fst.add_transition("q0", "s", "s", "qsh")
-fst.add_transition("qsh", "h", "h", "qes")
-fst.add_transition("qch", "h", "h", "qes")
-fst.add_transition("qch", "", "", "qs")
+fst.add_state("qsc", False)
+fst.add_transition("q0", "c", "c", "qsc")
+fst.add_transition("q0", "s", "s", "qsc")
+fst.add_transition("qsc", "h", "h", "qes")
 
 # y
 # #[CONSONANTS]y -> #[CONSONANTS]ies
@@ -44,19 +42,23 @@ fst.add_transition("q0", "f", "v", "qv")
 fst.add_transition("qv", "", "", "qes")
 fst.add_transition("qv", "e", "e", "qs")
 
+# normal nonens
+fst.add_state("qn", False)
+fst.add_set_transition("q0", NONSPECIAL, "qn")
+fst.add_transition("qn", "", "", "qs")
+# h but not [cs]h
+AZ_SC = AZ.replace("c", "").replace("s", "")
+fst.add_state("qh", False)
+fst.add_set_transition("q0", AZ_SC, "qh")
+fst.add_transition("qh", "h", "h", "qs")
 
-fst.run("bus")
-fst.run("marsh")
-fst.run("lunch")
-fst.run("tax")
-fst.run("blitz")
-fst.run("truss")
-fst.run("tuc")
-fst.run("city")
-fst.run("puppy")
-fst.run("ray")
-fst.run("boy")
-fst.run("wolf")
-fst.run("wife")
-fst.run("potato")
-fst.run("tomato")
+# e but not fe
+AZ_E = AZ.replace("f", "")
+fst.add_state("qe", False)
+fst.add_set_transition("q0", AZ_E, "qe")
+fst.add_transition("qe", "e", "e", "qs")
+
+with open("./test.txt") as f:
+    for t in f:
+        print(t.strip())
+        fst.run(t.strip())
